b0f39c66bf6d177e8e39f5c09d146f5b001c42ab
Beata Podgorska <beata.podgorska26@gmail.com>
2022-05-02 15:36:34 +0200

    Solved

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -1,6 +1,7 @@
 package com.epam.rd.autotasks;
 
 import java.util.Optional;
+import static java.lang.Math.abs;
 
 public enum Direction {
     N(0), NE(45), E(90), SE(135), S(180), SW(225), W(270), NW(315);
@@ -12,18 +13,34 @@ public enum Direction {
     private int degrees;
 
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        while(degrees >= 360) degrees = degrees - 360;
+        while(degrees < 0) degrees = 360 + degrees;
+
+        for( Direction dir : Direction.values()){
+            if(dir.degrees == degrees) return dir;
+        }
+        return null;
     }
 
     public static Direction closestToDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        while(degrees >= 360) degrees = degrees - 360;
+        while(degrees < 0) degrees = 360 + degrees;
+
+        for( Direction dir : Direction.values()){
+            if(dir.degrees -23 <= degrees && dir.degrees +22 >= degrees) return dir;
+        }
+        return null;
     }
 
     public Direction opposite() {
-        throw new UnsupportedOperationException();
+        int newDegrees = degrees + 180;
+        return ofDegrees(newDegrees);
     }
 
     public int differenceDegreesTo(Direction direction) {
-        throw new UnsupportedOperationException();
+        int newDegrees = abs(degrees - direction.degrees);
+        while(newDegrees > 180) newDegrees = abs(360 - newDegrees);
+        return newDegrees;
     }
+
 }
